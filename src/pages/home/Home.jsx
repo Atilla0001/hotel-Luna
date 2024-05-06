229e3556350114a15524f13db349656486b6b86b
Atilla0001 <toulouse663831@gmail.com>
2024-05-06 22:51:26 +0300

    12

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -61,7 +61,7 @@ const Home = () => {
 
 
 
-<<<<<<< HEAD
+
 <div className="container-fluid" 
 style={{ display:"flex",flexDirection:"column",background:"#BED3EA ", borderRadius:"50px",alignItems:"center"}}>
 <h1 style={{color:"#000 ", fontWeight:"600"}} id="titleDrinks">DINING & DRINKS & NIGHT</h1>
@@ -79,7 +79,7 @@ style={{ display:"flex",flexDirection:"column",background:"#BED3EA ", borderRadi
 =======
 <div className="container-fluid" style={{ display:"flex",justifyContent:"center",background:"#0069DB", borderRadius:"50px",opacity:"0.2"}}>
 <h1 >DINING & DRINKS</h1>
->>>>>>> 9d1cc363ebd0bb34b5e1d582a6328b384cb35d6a
+
 </div>
 
     </>
